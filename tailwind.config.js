81705094592be5de89c01327fe78177da30073f3
gx-ogura <yamato.ogura@gen-x.co.jp>
2025-02-12 01:44:23 +0900

    ui修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
@@ -7,7 +7,22 @@ export default {
         sans: ['Noto Sans JP', 'sans-serif'],
         display: ['Zen Kaku Gothic New', 'sans-serif'],
       },
+      animation: {
+        marquee: 'marquee 10s linear infinite',
+        marquee2: 'marquee2 10s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-25%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
     },
   },
+  variants: {},
   plugins: [],
 };
