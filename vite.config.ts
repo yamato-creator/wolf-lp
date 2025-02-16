1751f270526e098e86ac903a3e961681c8cb7de1
gx-ogura <yamato.ogura@gen-x.co.jp>
2025-02-16 16:12:47 +0900

    修正

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',
+  base: '/wolf-lp/',
   plugins: [react()],
   build: {
     outDir: 'dist',
