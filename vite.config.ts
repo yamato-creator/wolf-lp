cf45c350c3e4fe25f8e70ac1ac48d2794c8f93cb
gx-ogura <yamato.ogura@gen-x.co.jp>
2025-02-16 11:35:56 +0900

    修正

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',  // 相対パスに変更
+  base: process.env.NODE_ENV === 'production' ? '/wolf-lp/' : '/',  // GitHub Pagesのリポジトリ名に合わせて変更
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
